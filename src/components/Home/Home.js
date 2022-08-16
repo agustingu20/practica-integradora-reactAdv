1d8721c5c2a26008f188871b1ba934bd9066b02a
Agustin.gu <agustin.gu20@gmail.com>
2022-08-16 20:10:29 -0300

    Add bootstrap and home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+        <div>
+          hola
+            <link to='/search'><button type='button' className='btn btn-primary'></button></link>
+        </div>
+  );
+};
+
+export default Home;
